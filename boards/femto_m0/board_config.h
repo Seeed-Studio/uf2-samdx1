d05db0417ff22ce78b93708da11a218140c7f67f
LynnL4 <lht856@foxmail.com>
2019-09-20 05:58:54 +0000

    add board Seeeduino femto_mo define.

---
+++ boards/femto_m0/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Seeed Studio"
+#define PRODUCT_NAME "Seeeduino Femto"
+#define VOLUME_LABEL "Arduino"
+
+#define BOARD_ID "SAMD21G18A-Femto-v0"
+
+#define USB_VID 0x2886
+#define USB_PID 0x002C
+
+
+#endif
