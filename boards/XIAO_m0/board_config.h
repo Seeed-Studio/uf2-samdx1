90ff61102374b5432db9a724e1b496ce048b6667
LynnL4 <lht856@foxmail.com>
2019-11-27 14:35:21 +0800

    Add support for Seeeduino XIAO.

---
+++ boards/XIAO_m0/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+//#define CRYSTALLESS    0
+
+#define VENDOR_NAME "Seeed Studio"
+#define PRODUCT_NAME "Seeeduino XIAO"
+#define VOLUME_LABEL "Arduino"
+
+#define BOARD_ID "SAMD21G18A-XIAO-v0"
+
+#define USB_VID 0x2886
+#define USB_PID 0x002F
+
+#define LED_PIN PIN_PA17
+#define LED_TX_PIN PIN_PA19
+#define LED_RX_PIN PIN_PA18
+
+
+#endif
